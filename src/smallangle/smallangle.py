4892c9f3fcabf9b1ebf28dca15dfdae19f3845a0
BramvanZelst-gif <bvz.bram@gmail.com>
2024-11-15 16:10:36 +0100

    smallangle werkend
    de opdracht werkt zoals het moet

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -10,17 +10,38 @@ def smallangle_group():
 
 
 @smallangle_group.command
-@click.option("-n", "--number", default=10)
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="The amount of steps the interval is divided in",
+    type=int,
+    show_default=True,  # shows the default in --help
+)
 def sin(number):
-    x = np.linspace(0, 2 * pi, int(number))
+    """
+    Breaks the interval [0, 2pi] into a NUMBER of steps and calculates the sine for each one.
+    """
+    x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 
 @smallangle_group.command
-@click.option("-n", "--number", default=10)
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="The amount of steps the interval is divided in",
+    type=int,
+    show_default=True,  # shows the default in --help
+)
 def tan(number):
-    x = np.linspace(0, 2 * pi, int(number))
+    """
+    breaks the interval [0, 2pi] into a NUMBER steps and calculates the tangent for each one
+
+    """
+    x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
